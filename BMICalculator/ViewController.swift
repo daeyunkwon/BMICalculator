2d7976bed8efb83595bc622ba9de2524f06933a4
Daeyun <sweet_ray@naver.com>
2024-05-21 17:27:30 +0900

    Feat: 뷰 컴포넌트들의 초기 UI 구성 코드 구현

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -8,11 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var heightTextField: UITextField!
+    
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var weightTextField: UITextField!
+    
+    @IBOutlet var securityButton: UIButton!
+    @IBOutlet var randomButton: UIButton!
+    @IBOutlet var calculateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
+    
+    func configureUI() {
+        configureLabel()
+        configureTextField()
+        configureButton()
+    }
+    
+    func configureLabel() {
+        titleLabel.text = "BMI Calculator"
+        titleLabel.font = .systemFont(ofSize: 28, weight: .heavy)
+        
+        subTitleLabel.text = "당신의 BMI 지수를\n알려드릴게요."
+        subTitleLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        subTitleLabel.numberOfLines = 0
+        
+        heightLabel.text = "키가 어떻게 되시나요?"
+        heightLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+        
+        weightLabel.text = "몸무게가 어떻게 되시나요?"
+        weightLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+    }
+    
+    func configureTextField() {
+        heightTextField.layer.cornerRadius = 20
+        heightTextField.layer.borderColor = UIColor.darkGray.cgColor
+        heightTextField.layer.borderWidth = 2
+        heightTextField.borderStyle = .none
+        
+        weightTextField.layer.cornerRadius = 20
+        weightTextField.layer.borderColor = UIColor.darkGray.cgColor
+        weightTextField.layer.borderWidth = 2
+        weightTextField.borderStyle = .none
+    }
+    
+    func configureButton() {
+        securityButton.tintColor = .lightGray
+        
+        randomButton.setTitleColor(UIColor(red: 0.76, green: 0.07, blue: 0.12, alpha: 1.00), for: .normal)
+        randomButton.setTitle("랜덤으로 BMI 계산하기", for: .normal)
+        randomButton.titleLabel?.font = .systemFont(ofSize: 14)
+        randomButton.contentHorizontalAlignment = .right
+        
+        calculateButton.setTitle("결과 확인", for: .normal)
+        calculateButton.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        calculateButton.tintColor = .white
+        calculateButton.backgroundColor = .purple
+        calculateButton.layer.cornerRadius = 18
+    }
+    
+    
+    
+    
+    
+    
+    
 
 
 }
